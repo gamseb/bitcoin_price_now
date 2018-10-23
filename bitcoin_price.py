84db783a920991e595dc8cf8b612053925b0d1a5
gamseb <gamseb@gmail.com>
2018-10-23 21:12:28 +0200

    Added the EUR currency

--- bitcoin_price.py
+++ bitcoin_price.py
@@ -1,13 +1,19 @@
-#!/usr/bin/python
+#!/home/gamseb/anaconda3/bin/python3
 
 #This is a script that returns the price of one Bitcoin in US dollars
 import requests, json
 
 #Gets the api of https://www.bitcoinprice.com
-response = requests.get("https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC&tsyms=USD&e=CCCAGG")
+usd_response = requests.get("https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC&tsyms=USD&e=CCCAGG")
+euro_response = requests.get("https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC&tsyms=EUR&e=CCCAGG")
 #Converts to json
-a = response.json()
+usd = usd_response.json()
+euro = euro_response.json()
+bitcoin_symbol = usd['DISPLAY']['BTC']['USD']['FROMSYMBOL']
 #prints to the console
-print("*" * 30)
-print("The current price of one bitcoin is:")
-print("{} USD".format(a['RAW']['BTC']['USD']['PRICE']))
+print("*" * 36)
+print("The current price of one {}itcoin is:".format(bitcoin_symbol))
+print("{} USD".format(usd['RAW']['BTC']['USD']['PRICE']))
+print("{} EUR".format(euro['RAW']['BTC']['EUR']['PRICE']))
+print("*" * 36)
+
